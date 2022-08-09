a4648f19ecb68fac810984499a49f132a2edb3ec
santospatricia11 <patricia.pereira@academico.ifpb.edu.br>
2022-08-09 19:44:32 -0300

    Atualizações testes user

--- falacampus/src/test/java/br/edu/ifpb/dac/falacampus/presentation/control/UserControllerSystem.java
+++ falacampus/src/test/java/br/edu/ifpb/dac/falacampus/presentation/control/UserControllerSystem.java
@@ -37,6 +37,37 @@ class UserControllerSystem {
 		System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
 	}
+	@Test
+	void createUserWithInvalidFields() throws InterruptedException {
+		driver.get(URL);
+
+		WebElement name = driver.findElement(By.id("inputUserName"));
+		name.sendKeys("Patricia");
+
+		WebElement email = driver.findElement(By.id("inputEmail"));
+		email.sendKeys("patricia@pereira");
+
+		WebElement matricula = driver.findElement(By.id("inputRegistration"));
+		matricula.sendKeys("12345666999");
+
+		WebElement papel = driver.findElement(By.id("selectRole"));
+		Select select = new Select(papel);
+		select.selectByIndex(1);
+
+		WebElement senha = driver.findElement(By.id("inputPassword"));
+		senha.sendKeys("AAee#12345666999*");
+
+		WebElement departament = driver.findElement(By.id("inputIdDepartament"));
+		Select select1 = new Select(departament);
+		select1.selectByIndex(2);
+		
+		WebElement saveButton = driver.findElement(By.id("buttonSalvar"));
+		saveButton.click();
+
+		Thread.sleep(3000);
+		
+		driver.close();
+	}
 
 	@Test
 	void createUserWithValidFields() throws InterruptedException {
@@ -68,9 +99,40 @@ class UserControllerSystem {
 		Thread.sleep(3000);
 		
 		driver.close();
+	} 
+
+	@Test
+	void createUserWithInvalid() throws InterruptedException {
+		driver.get(URL);
+
+		WebElement name = driver.findElement(By.id("inputUserName"));
+		name.sendKeys("Patricia");
+
+		WebElement email = driver.findElement(By.id("inputEmail"));
+		email.sendKeys("patricia@pereira.com");
+
+		WebElement matricula = driver.findElement(By.id("inputRegistration"));
+		matricula.sendKeys("12345666999");
+
+		WebElement papel = driver.findElement(By.id("selectRole"));
+		Select select = new Select(papel);
+		select.selectByIndex(1);
+
+		WebElement senha = driver.findElement(By.id("inputPassword"));
+		senha.sendKeys("ee#12345666999");
+
+		WebElement departament = driver.findElement(By.id("inputIdDepartament"));
+		Select select1 = new Select(departament);
+		select1.selectByIndex(2);
+		
+		WebElement saveButton = driver.findElement(By.id("buttonSalvar"));
+		saveButton.click();
+
+		Thread.sleep(3000);
+		
+		driver.close();
 	}
 
-	
 	@Test
 	void cancelarButtonCreateUser() throws InterruptedException {
 		driver.get(URL);
@@ -79,9 +141,46 @@ class UserControllerSystem {
 		cancel.click();
 		
 		driver.navigate().to("http://localhost:3000");
-		Thread.sleep(3000);
+		
 
 	}
+	@Test
+	void createNovoUser() throws InterruptedException {
+		driver.get(URL);
+
+		driver.get("http://localhost:3000/viewUsers");
+		WebElement cadastrarNovoUser = driver.findElement(By.id("idNovoUser"));
+		Thread.sleep(3000);
+		cadastrarNovoUser.click();
+
+		WebElement name = driver.findElement(By.id("inputUserName"));
+		name.sendKeys("Patricia");
+
+		WebElement email = driver.findElement(By.id("inputEmail"));
+		email.sendKeys("patricia@pereira.com");
+
+		WebElement matricula = driver.findElement(By.id("inputRegistration"));
+		matricula.sendKeys("12345666999");
+
+		WebElement papel = driver.findElement(By.id("selectRole"));
+		Select select = new Select(papel);
+		select.selectByIndex(1);
+
+		WebElement senha = driver.findElement(By.id("inputPassword"));
+		senha.sendKeys("AAee#12345666999*");
+
+		WebElement departament = driver.findElement(By.id("inputIdDepartament"));
+		Select select1 = new Select(departament);
+		select1.selectByIndex(1);
+
+		WebElement saveButton = driver.findElement(By.id("buttonSalvar"));
+		saveButton.click();
+		driver.get("http://localhost:3000");
+		driver.close();
+	
+
+	}
+
 
 
 }
